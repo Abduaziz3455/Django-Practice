0a47f4bd9c380e390f221321c441b48b26ad3cfd
Abduaziz3455 <abduaziz3455@gmail.com>
2022-11-22 12:28:48 +0500

    heroku config

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^68r!+zcbox4vzwh8e20iv421oc49r++j1ukbt*0ovgbhll&t2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
